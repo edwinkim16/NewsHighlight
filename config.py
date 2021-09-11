a6a12ac1f4deb9b1273a0da3d546e559d1589db0
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-09-11 12:29:09 +0300

    setting up configurations

--- config.py
+++ config.py
@@ -9,3 +9,26 @@ class Config:
     ARTICLES_URL = 'https://newsapi.org/v2/everything?q=Apple&from=2021-09-11&sortBy=popularity&apiKey={}'
     HEADLINES_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+    
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}    
