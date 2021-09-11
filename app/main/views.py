e82594baa88f6b2540dbd335db05da16cd060e1b
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-09-11 13:18:37 +0300

    creating articles route

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,7 @@
 from flask import render_template
 from .import main
+from ..request import get_sources,get_articles
+
 
 @main.route('/')
 def index():
@@ -10,7 +12,18 @@ def index():
     
     
     title = 'Welcome to NewsWeb App'
+    news_sources = get_sources('general')
+    print(news_sources)
    
     
+    return render_template('index.html',title = title,sources =news_sources)
+
+@main.route('/articles/<id>')
+def articles(id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
     
-    return render_template('index.html',title = title)
+    articles = get_articles(id)
+    return render_template('articles.html',id = id, articles=articles)
